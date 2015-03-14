4366fd318749fb48420986bfc22dc003119ca912
paavopere <paavo@pere.fi>
2015-03-14 03:58:43 +0200

    Move super() calls to top of __init__ methods in deeps

--- src/deeps.py
+++ src/deeps.py
@@ -15,13 +15,6 @@ class Piece(object):
 
         self.square = None
 
-        # Pieces that are not explicitly multi_move-enabled will have
-        # multi_move=False
-        self.multi_move = getattr(self, 'multi_move', False)
-        # Pieces that do not have separate captures set will have moves set
-        # double as the captures set
-        self.captures = getattr(self, 'captures', getattr(self, 'moves', {}))
-
     def __repr__(self):
         return "{} {}".format("White" if self.color == self.C_WHITE else "Black",
                               self.__class__.__name__)
@@ -29,61 +22,78 @@ class Piece(object):
 
 class King(Piece):
     def __init__(self, color):
+        super().__init__(color)
+
+        self.multi_move = False
+
         self.moves = {
             (x, y) for x in (-1, 0, 1) for y in (-1, 0, 1)
         } - {(0, 0)}
-        super().__init__(color)
+
         #TODO: Implement castling
         #TODO: Implement check and checkmate
 
 
 class Queen(Piece):
     def __init__(self, color):
+        super().__init__(color)
+
+        self.multi_move = True
+
         self.moves = {
             (x, y) for x in (-1, 0, 1) for y in (-1, 0, 1)
         } - {(0, 0)}
-        self.multi_move = True
-        super().__init__(color)
 
 
 class Rook(Piece):
     def __init__(self, color):
+        super().__init__(color)
+
+        self.multi_move = True
+
         self.moves = {
             (1, 0), (0, 1), (-1, 0), (0, -1)
         }
-        self.multi_move = True
-        super().__init__(color)
         #TODO: Implement castling
 
 
 class Bishop(Piece):
     def __init__(self, color):
+        super().__init__(color)
+
+        self.multi_move = True
+
         self.moves = {
             (x, y) for x in (-1, 1) for y in (-1, 1)
         }
-        self.multi_move = True
-        super().__init__(color)
 
 
 class Knight(Piece):
     def __init__(self, color):
+        super().__init__(color)
+
+        self.multi_move = False
+
         self.moves = {
             (x, y) for x in (-2, 2) for y in (-1, 1)
         } | {
             (x, y) for x in (-1, 1) for y in (-2, 2)
         }
-        super().__init__(color)
 
 
 class Pawn(Piece):
     def __init__(self, color):
+        super().__init__(color)
+
+        self.multi_move = False
+
         if color == Piece.C_WHITE:
             self.moves = {(0, 1)}
             self.captures = {(1, 1), (-1, 1)}
         elif color == Piece.C_BLACK:
             self.moves = {(0, -1)}
             self.captures = {(1, -1), (-1, -1)}
-        super().__init__(color)
+
         #TODO: Implement en passant
 
 
