def777df5754681dbd823e5a773ddaa9873377ad
paavopere <paavo@pere.fi>
2016-07-27 03:16:01 +0300

    implement Game with position, position from FEN, and Game to a basic board representation

--- deepes.py
+++ deepes.py
@@ -1,3 +1,48 @@
 
 class Game:
-    pass
+    def __init__(self, fen=None):
+        if fen is not None:
+            self.position = self.position_from_fen(fen)
+        else:
+            self.position = self.starting_position()
+
+    def __repr__(self):
+        return '{}()'.format(self.__class__.__name__)
+
+    @staticmethod
+    def starting_position():
+        pos = (
+            tuple(c for c in 'rnbqkbnr'),
+            ('p',) * 8,
+            ('.',) * 8,
+            ('.',) * 8,
+            ('.',) * 8,
+            ('.',) * 8,
+            ('P',) * 8,
+            tuple(c for c in 'RNBQKBNR')
+        )
+        return pos
+
+    @staticmethod
+    def position_from_fen(fen):
+        """
+        According to '1. Piece placement' in
+        https://en.wikipedia.org/w/index.php?title=Forsyth%E2%80%93Edwards_Notation&oldid=707127024#Definition
+        """
+        fen_pieces = fen.split(' ')[0]
+        fen_ranks = fen_pieces.split('/')
+
+        position = []
+        for fr in fen_ranks:
+            rank = []
+            for square in fr:
+                if square.isdigit():
+                    rank.extend('.' * int(square))
+                else:
+                    rank.append(square)
+            position.append(tuple(rank))
+
+        return tuple(position)
+
+    def basic_board(self):
+        return '\n'.join(''.join(piece for piece in rank) for rank in self.position)
--- test_deepes.py
+++ test_deepes.py
@@ -1,5 +1,81 @@
-import deepes
+from deepes import Game
 
 
-def test_deepes_init():
-    deepes.Game()
+STARTING_POSITION = (
+    ('r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'),
+    ('p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'),
+    ('.', '.', '.', '.', '.', '.', '.', '.'),
+    ('.', '.', '.', '.', '.', '.', '.', '.'),
+    ('.', '.', '.', '.', '.', '.', '.', '.'),
+    ('.', '.', '.', '.', '.', '.', '.', '.'),
+    ('P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'),
+    ('R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'),
+)
+POSITION_AFTER_E4 = (
+    ('r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'),
+    ('p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'),
+    ('.', '.', '.', '.', '.', '.', '.', '.'),
+    ('.', '.', '.', '.', '.', '.', '.', '.'),
+    ('.', '.', '.', '.', 'P', '.', '.', '.'),
+    ('.', '.', '.', '.', '.', '.', '.', '.'),
+    ('P', 'P', 'P', 'P', '.', 'P', 'P', 'P'),
+    ('R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'),
+)
+STARTING_FEN = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
+FEN_AFTER_E4 = 'rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1'
+STARTING_BBOARD = ('rnbqkbnr\n'
+                   'pppppppp\n'
+                   '........\n'
+                   '........\n'
+                   '........\n'
+                   '........\n'
+                   'PPPPPPPP\n'
+                   'RNBQKBNR')
+BBOARD_AFTER_E4 = ('rnbqkbnr\n'
+                   'pppppppp\n'
+                   '........\n'
+                   '........\n'
+                   '....P...\n'
+                   '........\n'
+                   'PPPP.PPP\n'
+                   'RNBQKBNR')
+
+
+def test_game_initializes():
+    assert type(Game()) == Game
+
+
+def test_default_game_repr():
+    assert repr(Game()) == 'Game()'
+
+
+def test_starting_position():
+    assert Game.starting_position() == STARTING_POSITION
+
+
+def test_init_position():
+    assert Game().position == STARTING_POSITION
+
+
+def test_initializes_with_fen():
+    assert Game(fen=STARTING_FEN)
+
+
+def test_starting_fen_gives_starting_position():
+    assert Game(fen=STARTING_FEN).position == STARTING_POSITION
+
+
+def test_position_from_fen_after_e4():
+    assert Game(fen=FEN_AFTER_E4).position == POSITION_AFTER_E4
+
+
+def test_starting_basic_board():
+    assert Game().basic_board() == STARTING_BBOARD
+
+
+def test_basic_board_from_fen_after_e4():
+    assert Game(fen=FEN_AFTER_E4).basic_board() == BBOARD_AFTER_E4
+
+
+if __name__ == '__main__':
+    print(STARTING_BBOARD)
